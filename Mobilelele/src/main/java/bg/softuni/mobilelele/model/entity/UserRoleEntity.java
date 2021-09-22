3102fa8feb7c8b9f0a2c85b1fe9d2f4a5db09d75
nzlateva <72468080+nzlateva@users.noreply.github.com>
2021-09-22 13:12:02 +0300

    Added Getters and Setters to UserRoleEntity.

--- Mobilelele/src/main/java/bg/softuni/mobilelele/model/entity/UserRoleEntity.java
+++ Mobilelele/src/main/java/bg/softuni/mobilelele/model/entity/UserRoleEntity.java
@@ -14,4 +14,22 @@ public class UserRoleEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserRoleEnum role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public UserRoleEntity setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public UserRoleEnum getRole() {
+        return role;
+    }
+
+    public UserRoleEntity setRole(UserRoleEnum role) {
+        this.role = role;
+        return this;
+    }
 }
